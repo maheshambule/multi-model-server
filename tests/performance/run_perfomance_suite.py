5f5a6d133721d30fc455f88b9dc9405fccbd520c
maheshambule <ambulemahesh@gmail.com>
2020-06-17 22:44:42 +0530

    compare changes

--- tests/performance/run_perfomance_suite.py
+++ tests/performance/run_perfomance_suite.py
@@ -26,10 +26,14 @@ import pathlib
 import subprocess
 import yaml
 import requests
+import psutil
 from subprocess import PIPE, STDOUT
 import csv
 from tqdm import tqdm
+import pandas as pd
+import boto3
 from junitparser import TestCase, TestSuite, JUnitXml, Skipped, Error, Failure
+
 
 logger = logging.getLogger(__name__)
 code = 0
@@ -93,16 +97,123 @@ def get_options(artifacts_dir, jmeter_path=None):
     return options_str
 
 
-def compare_artifacts(dir1, dir2):
+def get_folder_names(dir1):
+    return [di for di in os.listdir(dir1) if os.path.isdir(os.path.join(dir1, di))]
+
+
+def get_latest_dir(dir1, env_id):
+    max_ts = 0
+    latest_run = ''
+    for run_name in get_folder_names(dir1):
+        run_name_list = run_name.split('_')
+        if env_id == run_name_list[0]:
+            if int(run_name_list[1]) > max_ts:
+                max_ts = int(run_name_list[2])
+                latest_run = run_name
+    return os.path.join(dir1, latest_run)
+
+
+def get_env_info():
+    {"platform" : psutil.os.sys.platform,
+     }
+
+
+def upload_to_s3(local_directory, destination, bucket="regression-reports-123"):
+
+    client = boto3.client('s3')
+
+    # enumerate local files recursively
+    for root, dirs, files in os.walk(local_directory):
+        for filename in files:
+            # construct the full local path
+            local_path = os.path.join(root, filename)
+
+            # construct the full s3 path
+            relative_path = os.path.relpath(local_path, local_directory)
+            s3_path = os.path.join(destination, relative_path)
+            logger.info('Searching "%s" in "%s"' % (s3_path, bucket))
+            try:
+                client.head_object(Bucket=bucket, Key=s3_path)
+                logger.info("Path found on S3! Skipping %s..." % s3_path)
+            except:
+                logger.info("Uploading %s..." % s3_path)
+                client.upload_file(local_path, bucket, s3_path)
+
+
+def download_s3_files(bucket="regression-reports-123"):
+    # initiate s3 resource
+    s3 = boto3.resource('s3')
+    # select bucket
+    my_bucket = s3.Bucket(bucket)
+    # download file into current directory
+    for s3_object in my_bucket.objects.all():
+        filename = s3_object.key
+        try:
+            my_bucket.download_file(s3_object.key, filename)
+        except:
+            pass
+
+
+def compare_artifacts(dir1, dir2, out_dir):
     ##compare metrics test case wise
 
-    ##plot graphs
-    pass
+    dir1, dir2 = dir1.strip(), dir2.strip()
+
+    if not os.path.exists(dir1):
+        raise Exception("The path {} does not exit".format(dir1))
+
+    if not os.path.exists(dir2):
+        raise Exception("The path {} does not exit".format(dir2))
+
+    sub_dirs_1 = [x[0].rsplit('/',1)[1] for x in os.walk(dir1)]
+    sub_dirs_2 = [x[0].rsplit('/',1)[1] for x in os.walk(dir2)]
+
+    aggregates = ["mean", "max", "min"]
+    header = ["test_suite", "metric", "run1", "run2", "diff"]
+    rows = [header]
+    print(sub_dirs_1)
+    print(sub_dirs_2)
+    for sub_dir1 in sub_dirs_1:
+        if sub_dir1 in sub_dirs_2:
+
+            metrics_file1 = glob.glob("{}/{}/SAlogs_*".format(dir1, sub_dir1))
+            metrics_file2 = glob.glob("{}/{}/SAlogs_*".format(dir2, sub_dir1))
+
+            if not (metrics_file1 and metrics_file2):
+
+                metrics_file1 = glob.glob("{}/{}/local_*".format(dir1, sub_dir1))
+                metrics_file2 = glob.glob("{}/{}/local_*".format(dir2, sub_dir1))
+
+                if not (metrics_file1 and metrics_file2):
+                    break
 
 
+            metrics1 = pd.read_csv(metrics_file1[0])
+            metrics2 = pd.read_csv(metrics_file2[0])
+
+            for col in metrics1.columns:
+                for agg_func in aggregates:
+                    val1 = getattr(metrics1[str(col)], agg_func)()
+                    val2 = getattr(metrics2[str(col)], agg_func)()
+                    diff = val2 - val1
+
+                    # if diff > set_criteria:
+                    #     raise Exception()
+
+                    name = "{}_{}".format(agg_func, str(col))
+                    rows.append([sub_dir1, name, val1, val2, diff])
+
+    # run_name1 = os.path.basename(os.path.normpath(dir1))
+    # run_name2 = os.path.basename(os.path.normpath(dir2))
+
+    out_path = "{}/comparison.report".format(out_dir)
+    with open(out_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerows(rows)
 
 
-def run_test_suite(artifacts_dir, test_dir, pattern, jmeter_path, monitoring_server):
+def run_test_suite(artifacts_dir, test_dir, pattern, jmeter_path, monitoring_server, env_name):
     if os.path.exists(artifacts_dir):
         raise Exception("Artifacts dir '{}' already exists. Provide different one.".format(artifacts_dir))
 
@@ -169,7 +280,7 @@ def run_test_suite(artifacts_dir, test_dir, pattern, jmeter_path, monitoring_ser
                     # TODO Fix html report with system_err
                     # tc.system_err = err_txt[:-4]
                     ts.add_testcase(tc)
-                    out_report_row.extend([name, result._tag])
+                    out_report_row.extend([name, result._tag if result else "passed"])
                     out_report_rows.append(out_report_row)
 
         else:
@@ -208,30 +319,48 @@ def run_test_suite(artifacts_dir, test_dir, pattern, jmeter_path, monitoring_ser
 
         csv_writer.writerows(out_report_rows)
 
+    latest_dir = get_latest_dir('/tmp/data/', env_name)
 
+    path = pathlib.Path(artifacts_dir)
+    print(path.parent)
+
+    commit_id = ""
+
+    new_name = "{}/{}_{}_{}/".format(path.parent, env_name, commit_id, suite_start)
+
+    os.rename(artifacts_dir, new_name)
+    compare_artifacts(artifacts_dir, latest_dir, artifacts_dir)
+
+    #upload_to_s3(artifacts_dir, '/{}_{}/'.format(env_name, suite_start))
 
     if junit_xml.errors or junit_xml.failures or junit_xml.skipped:
         sys.exit(3)
 
 
-if __name__ == "__main__":
-    logging.basicConfig(stream=sys.stdout, format="%(message)s", level=logging.INFO)
-    parser = argparse.ArgumentParser(prog='run_perfomance_suite.py', description='Performance Test Suite Runner')
-    parser.add_argument('-a', '--artifacts-dir', nargs=1, type=str, dest='artifacts', required=True,
-                           help='A artifacts directory')
+compare_artifacts('/Users/mahesh/tmp/data/run6', '/Users/mahesh/tmp/data/run5', '/Users/mahesh/tmp/data/run6')
 
-    parser.add_argument('-d', '--test-dir', nargs=1, type=str, dest='test_dir', default=[None],
-                           help='A test dir')
 
-    parser.add_argument('-p', '--pattern', nargs=1, type=str, dest='pattern', default=["*.yaml"],
-                           help='Test case file name pattern. example *.yaml')
-
-    parser.add_argument('-j', '--jmeter-path', nargs=1, type=str, dest='jmeter_path', default=[None],
-                        help='JMeter executable bin path')
-
-    parser.add_argument('-m', '--monitoring-server', nargs=1, type=bool, dest='monitoring_server', default=[True],
-                        help='Whether to start monitoring server')
-
-    args = parser.parse_args()
-    run_test_suite(args.artifacts[0], args.test_dir[0], args.pattern[0], args.jmeter_path[0],
-                   args.monitoring_server[0])
+# if __name__ == "__main__":
+#     logging.basicConfig(stream=sys.stdout, format="%(message)s", level=logging.INFO)
+#     parser = argparse.ArgumentParser(prog='run_perfomance_suite.py', description='Performance Test Suite Runner')
+#     parser.add_argument('-a', '--artifacts-dir', nargs=1, type=str, dest='artifacts', required=True,
+#                            help='A artifacts directory')
+#
+#     parser.add_argument('-e', '--env-name', nargs=1, type=bool, dest='env_name', default=[socket.gethostname()],
+#                         help='environment on which MMS server is running')
+#
+#     parser.add_argument('-d', '--test-dir', nargs=1, type=str, dest='test_dir', default=[None],
+#                            help='A test dir')
+#
+#     parser.add_argument('-p', '--pattern', nargs=1, type=str, dest='pattern', default=["*.yaml"],
+#                            help='Test case file name pattern. example *.yaml')
+#
+#     parser.add_argument('-j', '--jmeter-path', nargs=1, type=str, dest='jmeter_path', default=[None],
+#                         help='JMeter executable bin path')
+#
+#     parser.add_argument('-m', '--monitoring-server', nargs=1, type=bool, dest='monitoring_server', default=[True],
+#                         help='Whether to start monitoring server')
+#
+#     args = parser.parse_args()
+#     run_test_suite(args.artifacts[0], args.test_dir[0], args.pattern[0], args.jmeter_path[0],
+#                    args.monitoring_server[0], args.env_name[0])
